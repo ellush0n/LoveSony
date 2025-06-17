f855fb376fb36f56f5b52c5fe08be308f5359f0e
Ilya Berezov <berezovila1@gmail.com>
2025-06-17 13:22:55 +0300

    3

---
+++ src/index.js
@@ -0,0 +1,12 @@
+     import React from 'react';
+     import ReactDOM from 'react-dom';
+     import App from './App'; // Убедитесь, что у вас есть файл App.js в той же папке
+     import './index.css'; // Если у вас есть стили
+
+     ReactDOM.render(
+       <React.StrictMode>
+         <App />
+       </React.StrictMode>,
+       document.getElementById('root')
+     );
+     
